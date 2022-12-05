3628b208e1539b52fbda0285a298f1d2bcd4f54e
Bram Jansen <39745649+minxae@users.noreply.github.com>
2022-12-05 23:56:38 +0100

    added fragments  for main activity

--- LeagueBuddy/app/src/main/java/com/leaguebuddy/MainActivity.kt
+++ LeagueBuddy/app/src/main/java/com/leaguebuddy/MainActivity.kt
@@ -2,11 +2,24 @@ package com.leaguebuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.leaguebuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-            supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,  fragment)
+        fragmentTransaction.commit();
     }
 }
