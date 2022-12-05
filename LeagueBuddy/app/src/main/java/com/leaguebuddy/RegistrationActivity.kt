545867df96b33f2424172126c0ba408561d3157f
SamEchten <SamEchten@users.noreply.github.com>
2022-12-05 14:29:09 +0100

    Registration update

--- LeagueBuddy/app/src/main/java/com/leaguebuddy/RegistrationActivity.kt
+++ LeagueBuddy/app/src/main/java/com/leaguebuddy/RegistrationActivity.kt
@@ -1,11 +1,25 @@
 package com.leaguebuddy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
-class RegistrationActivity : AppCompatActivity() {
+class RegistrationActivity : Activity() {
+    private lateinit var etUserName: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etRepeatPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        etUserName = findViewById(R.id.etUserName)
+        etPassword = findViewById(R.id.etPassword)
+        etRepeatPassword = findViewById(R.id.etRepeatPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
     }
 }
