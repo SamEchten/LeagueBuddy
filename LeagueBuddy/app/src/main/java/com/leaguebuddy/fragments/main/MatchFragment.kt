eceab0bd86a2ee8919cc93346387a9ca98177061
Bram Jansen <39745649+minxae@users.noreply.github.com>
2022-12-06 17:12:16 +0100

    Created match screen and added all fragments for it

--- LeagueBuddy/app/src/main/java/com/leaguebuddy/fragments/main/MatchFragment.kt
+++ LeagueBuddy/app/src/main/java/com/leaguebuddy/fragments/main/MatchFragment.kt
@@ -1,17 +1,47 @@
 package com.leaguebuddy.fragments.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.leaguebuddy.MainActivity
 import com.leaguebuddy.R
+import org.w3c.dom.Text
 
 
 class MatchFragment : Fragment() {
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var mainActivity: MainActivity
+    private lateinit var matchStatsBtn: Button
+    private lateinit var spellTimerBtn: Button
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainActivity = activity as MainActivity
+        replaceFragment(MatchStatsFragment())
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+        matchStatsBtn = view.findViewById(R.id.btnMatchStats)
+        spellTimerBtn = view.findViewById(R.id.btnSpellTimer)
+
+        matchStatsBtn.setOnClickListener{
+            replaceFragment(MatchStatsFragment())
+            matchStatsBtn.backgroundTintList = ContextCompat.getColorStateList(mainActivity, R.color.blueAccent)
+            spellTimerBtn.backgroundTintList = ContextCompat.getColorStateList(mainActivity, R.color.greyAccent)
+        }
+
+        spellTimerBtn.setOnClickListener{
+            replaceFragment(SpellTimerFragment())
+            matchStatsBtn.backgroundTintList = ContextCompat.getColorStateList(mainActivity, R.color.greyAccent)
+            spellTimerBtn.backgroundTintList = ContextCompat.getColorStateList(mainActivity, R.color.blueAccent)
+        }
+
     }
 
     override fun onCreateView(
@@ -21,4 +51,13 @@ class MatchFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_match, container, false)
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        with(childFragmentManager.beginTransaction()) {
+            replace(R.id.flMatchFragmentHolder, fragment)
+            commit()
+        }
+    }
+
+
 }
